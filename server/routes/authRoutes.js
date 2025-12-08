ad0957c64fb5ce890ef568aa599d330f8d29f9a6
Joel Brook <brook.jl@gmail.com>
2025-12-07 23:17:51 -0500

    Renamed to buzz

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { register, login, logout } = require('../controllers/authController');
-
+//yy
 const router = express.Router();
 
 router.post('/register', register);
